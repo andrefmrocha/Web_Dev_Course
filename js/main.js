443f80151321c9a5fc02748ea0acd5513293f1ca
andrefmrocha <andrefmrocha@live.com.pt>
2018-06-25 16:27:23 +0100

    Testing some jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-var btn = document.getElementById("go-button");
+$(document).ready(function () {
+    // alert("loaded");
 
-function buttonClick()
-{
-    console.log("Button clicked!");
-    var customText = document.getElementsByClassName("my-input");
-    var result = document.getElementById("text");
-    var textArea = document.getElementsByClassName("my-text-area")
-    result.innerHTML = "You want to search for " + customText[0].value + "<br/>";
-    result.innerHTML+="With the intent of " + textArea[0].value;
-}
-
-btn.addEventListener("click",buttonClick);
-
+    $('#text').html("This is now my text");
+    $('.my-input').val("New input val");
+    
+});
