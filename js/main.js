361e05f0edeb3c9d11c0ac2686eea273ba7f48e6
andrefmrocha <andrefmrocha@live.com.pt>
2018-06-25 13:08:44 +0100

    Using some more document methods

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+var btn = document.getElementById("go-button");
+
 function buttonClick()
 {
     console.log("Button clicked!");
-    btn.removeEventListener("click",buttonClick);
-    document.getElementById("text").innerText = "Don't do it"
+    var customText = document.getElementsByClassName("my-input");
+    var result = document.getElementById("text");
+    var textArea = document.getElementsByClassName("my-text-area")
+    result.innerHTML = "You want to search for " + customText[0].value + "<br/>";
+    result.innerHTML+="With the intent of " + textArea[0].value;
 }
 
-var btn = document.getElementById("go-button");
-
 btn.addEventListener("click",buttonClick);
+
