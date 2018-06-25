dc6b905c4dabe7140fd5beb517845e3759435ca1
andrefmrocha <andrefmrocha@live.com.pt>
2018-06-25 16:54:45 +0100

    Event Handlers on jQuery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     // alert("loaded");
-
+    var el = document.getElementById('text');
+    console.log("El in Vanilla JS", el);
+    console.log("EL in jQuery", $(el));
     $('#text').html("This is now my text");
     $('.my-input').val("New input val");
-    
+    $('#go-button').on('mouseenter',function () {
+        console.log("Button clicked!");
+    });
+
 });
