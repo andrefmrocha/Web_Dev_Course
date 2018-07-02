636467ab41da4b0aa3ce717beacd5e462b896efd
andrefmrocha <andrefmrocha@live.com.pt>
2018-07-02 23:07:54 +0100

    Created a dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    // alert("loaded");
-    var el = document.getElementById('text');
-    console.log("El in Vanilla JS", el);
-    console.log("EL in jQuery", $(el));
-    $('#text').html("This is now my text");
-    $('.my-input').val("New input val");
-    $('#go-button').on('mouseenter',function () {
-        console.log("Button clicked!");
+    $('[data-trigger = "dropdown"]').on('mouseenter', function () {
+        var submenu = $(this).parent().find('.submenu');
+        submenu.addClass('active');
+        $('.profile-menu').on('mouseleave', function () {
+            submenu.removeClass('active');
+
+        })
     });
+
+
 
 });
