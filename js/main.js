1cbc4cbe77553598ba295717608fa9e83ce0472c
andrefmrocha <andrefmrocha@live.com.pt>
2018-07-05 15:25:36 +0100

    Prevention methods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    var el = document.getElementById('text');
+
+    $('[href="https://google.com" ]').on('click', function (event) {
+       console.log('Linking to google?');
+       event.preventDefault();
+       // Returning a bool can prevent an action from happening
+
+    });
     $('[data-trigger = "dropdown"]').on('mouseenter', function () {
         var submenu = $(this).parent().find('.submenu');
         submenu.addClass('active');
@@ -8,20 +16,24 @@ $(document).ready(function () {
     });
 
     $('#prepend, #append, #replace').on('click', function (event) {
+        event.preventDefault();
         var el = $(event.currentTarget);
         var action = el.attr('id');
+        var content = $('.text').val()
         if (action == "prepend")
         {
-
+            $('#main').prepend('<a href = "#">' + content + '</a>');
         }
         else if(action == "append")
         {
-
+            $('#main').append('<a href = "#">' + content + '</a>');
         }
         else if (action == "replace")
         {
-
+            $('#main').html('<a href = "#">' + content + '</a>');
         }
+
+        $('.text').val('');
     });
 
 
