5fdcd6c0bc40036c9975fd41353e96d514179c33
andrefmrocha <andrefmrocha@live.com.pt>
2018-07-08 14:24:46 +0100

    Find method

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
-    socks(".my-selector").addClass("MYCLASS");
-
+$(function () {
     var el = document.getElementById('text');
+
+    $('#main-header').find('p').addClass('HAH'); // Since it doesnt find any p's in the main header it doesnt do anything
+    $('#main').find('p').addClass('HAH'); // The p element in main gets the Class
+
     $(document).on('contextmenu', function () {
         return false;
     });
